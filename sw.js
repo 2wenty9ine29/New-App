6c70d16a02648d25380982c9713599ab3a77e313
2wenty9ine29 <stephenbaaye123456ab@gmail.com>
2026-09-08 18:26:35 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "price-list-sales-calculator-v5-5-1";
+const CACHE = "price-list-sales-calculator-v5-5-1-r2";
 const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
